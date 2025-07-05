e5df597ecbf0584d20ba98533f1396da03a38fb1
olaniyigeorge <olaniyigeorge77@gmail.com>
2025-07-05 19:50:27 +0100

    Added row_number and rank for propertty based on booing number count

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -14,7 +14,8 @@ SELECT
     p.property_id,
     p.name,
     COUNT(b.booking_id) AS total_bookings,
-    ROW_NUMBER() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank
+    ROW_NUMBER() OVER (ORDER BY COUNT(b.booking_id) DESC) AS row_number,
+    RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank
 FROM Property p
 LEFT JOIN Booking b ON p.property_id = b.property_id
 GROUP BY p.property_id, p.name;
