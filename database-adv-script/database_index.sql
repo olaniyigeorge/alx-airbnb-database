19ccf25474271ab9586c0d666f7fb88ac9272083
olaniyigeorge <olaniyigeorge77@gmail.com>
2025-07-05 19:57:30 +0100

    xplain query after adding index

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -9,3 +9,10 @@ CREATE INDEX idx_booking_start_date ON Booking(start_date);
 CREATE INDEX idx_property_location ON Property(location);
 CREATE INDEX idx_property_price ON Property(price);
 
+
+
+-- Monitoring the performance of the index on Booking table
+EXPLAIN ANALYSE
+SELECT *
+FROM Booking
+WHERE start_date BETWEEN '2025-07-01' AND '2025-07-31';
