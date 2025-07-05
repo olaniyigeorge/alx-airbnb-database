42c9b075d4711f6265b95ffa591cd37fa2ab3073
olaniyigeorge <olaniyigeorge77@gmail.com>
2025-07-05 19:48:49 +0100

    Added order by to join queries

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -1,7 +1,3 @@
-
-
-
-
 -- Total bookings per user
 SELECT
     u.user_id,
@@ -18,7 +14,7 @@ SELECT
     p.property_id,
     p.name,
     COUNT(b.booking_id) AS total_bookings,
-    RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank
+    ROW_NUMBER() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank
 FROM Property p
 LEFT JOIN Booking b ON p.property_id = b.property_id
 GROUP BY p.property_id, p.name;
